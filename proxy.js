7e68047dd214a7ffc7d2f89c6f6e59a5d60acff2
shiqstone <shiqstone@gmail.com>
2025-01-05 13:23:17 +0800

    update tools

--- proxy.js
+++ proxy.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
